03fe9b58608fdbacc0a744632177692a2e3d2fdd
matilein <111425698+matilein@users.noreply.github.com>
2023-08-15 10:56:56 -0300

    Add files via upload

---
+++ moduloPoint.hs
@@ -0,0 +1,11 @@
+module Point ( Point, newP, difP)
+   where
+import Data.Text.Array (new)
+
+data Point = Poi Int Int deriving (Eq, Show)
+
+newP :: Int -> Int -> Point
+newP x y = (Poi x y)
+
+difP :: Point -> Point -> Float  -- distancia absoluta
+difP (Poi x1 y1) (Poi x2 y2) = sqrt  $ fromIntegral ((x1 - x2)^2 + (y1 - y2)^2)
