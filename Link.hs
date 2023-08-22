76ff2cc18b770c687bb60f5fe6a500e43e23eff5
martinbianchi0 <142119878+martinbianchi0@users.noreply.github.com>
2023-08-22 20:18:42 -0300

    Add files via upload

--- Link.hs
+++ Link.hs
@@ -1,12 +1,13 @@
 module Link ( Link, newL, linksL, connectsL, capacityL, delayL )
    where
+
 import Point
 import City
 import Quality
 data Link = Lin City City Quality deriving (Eq, Show)
 
 newL :: City -> City -> Quality -> Link -- genera un link entre dos ciudades distintas
-newL city1 city2 quality = (Lin city1 city2 quality)
+newL city1 city2 quality = Lin city1 city2 quality
 
 connectsL :: City -> Link -> Bool   -- indica si esta ciudad es parte de este link
 connectsL cityTarget (Lin city1 city2 _) | cityTarget==city1 = True
@@ -21,5 +22,5 @@ linksL cityTarget1 cityTarget2 (Lin city1 city2 _)  | cityTarget1==city1 && city
 capacityL :: Link -> Int
 capacityL (Lin _ _ quality) = capacityQ(quality)
 
-delayL :: Link -> Float     -- la demora que sufre una conexion en este canal
+delayL :: Link -> Float     -- la demora que sufre una conexión en este canal
 delayL (Lin city1 city2 quality) = delayQ(quality) * (distanceC city1 city2)
--- Tunel.hs
+++ Tunel.hs
@@ -1,27 +1,26 @@
-module Tunel ( Tunel, newT, connectsT, usesT )
-   where 
-
-import Link (Link(..))
+module Tunel ( Tunel, newT, connectsT, usesT, delayT )
+   where
+import Link
 import Point
-import Quality (Quality(..))
+import Quality
 import City
+import qualified Data.Type.Bool as True
 
 data Tunel = Tun [Link] deriving (Eq, Show)
 
 newT :: [Link] -> Tunel
-newT links = Tun links
+newT [] = error"Tunel vacío"
+newT thelinks = Tun thelinks
 
-connectsT :: City -> City -> Tunel -> Bool -- indica si este tunel conceta estas dos ciudades distintas
-connectsT city1 city2 (Tun links) | any (connects links city1 city2) = True
-                                  | otherwise = False
-connects :: Link -> City -> City -> Bool
-connects (Lin cityone citytwo _) = city1 == cityone && city2 == citytwo || city2 == cityone && city1 == citytwo
+connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
+connectsT city1 city2 (Tun [link]) = connectsL city1 link && connectsL city2 link 
+connectsT city1 city2 (Tun [link1,link2]) = connectsL city1 link1 && not(connectsL city1 link2) && not(connectsL city2 link1) && connectsL city2 link2
+connectsT city1 city2 (Tun links) = connectsL city1 (links!!0) && not(connectsL city1 (links!!1)) && connectsL city2 (last links) && not(connectsL city2 (links !! (length links -2)))
 
+delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
+delayT (Tun thelinks) =
+    sum delays
+     where delays = map delayL thelinks
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
 usesT link (Tun thelinks) = link `elem` thelinks
-
-delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
-delayT tunel = sum $ map demora tunel
-demora :: Link -> Float
-demora (Lin _ _ (Qua _ _ demora)) = demora
