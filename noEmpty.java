717d018177192ff09eeb80d61edab7a680889513
martinbianchi0 <142119878+martinbianchi0@users.noreply.github.com>
2023-09-15 17:30:28 -0300

    Update noEmpty.java

--- noEmpty.java
+++ noEmpty.java
@@ -1,13 +1,1 @@
-package queue;
 
-import java.util.LinkedList;
-
-public class noEmpty extends status {
-	
-    public Object take(LinkedList<Object> elements) {
-    	 return elements.removeFirst();
-	}
-    public Object head(LinkedList<Object> elements) {
-   	 	 return elements.getFirst();
-	}
-}
